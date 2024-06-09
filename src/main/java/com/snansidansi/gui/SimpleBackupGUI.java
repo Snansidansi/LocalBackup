bbf96a599c97a7fcaaa057f9b8bfd063abd90412
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2024-06-09 09:53:41 +0200

    Created a new main class + methode and a basic JavaFX window. Also adjusted build.gradle to new main class.
    The main class now uses args to determine if it should just run a backup or launch the GUI. The old main class has been removed.

---
+++ src/main/java/com/snansidansi/gui/SimpleBackupGUI.java
@@ -0,0 +1,12 @@
+package com.snansidansi.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SimpleBackupGUI extends Application {
+    @Override
+    public void start(Stage window) throws Exception {
+        window.setTitle("Simple Backup");
+        window.show();
+    }
+}
