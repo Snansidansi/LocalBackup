bb4c8807f536a4384470160dbb97fa3ef19ca5d3
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2024-06-20 19:57:16 +0200

    Added delete- and export log methods.

--- src/main/java/com/snansidansi/gui/controller/LogController.java
+++ src/main/java/com/snansidansi/gui/controller/LogController.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -21,7 +22,9 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class LogController {
-    File currentFile = null;
+    File currendDisplayedFile = null;
+    Boolean deletePressedOnce = false;
+    String lastSelectedExportPath = "";
 
     @FXML
     BorderPane mainBorderPane;
@@ -45,6 +48,9 @@ public class LogController {
     Label noLogFilesLabel;
 
     @FXML
+    Label confirmDeleteLabel;
+
+    @FXML
     public void initialize() {
         ToggleGroup logTypeTGroup = new ToggleGroup();
         this.errorLogTButton.setToggleGroup(logTypeTGroup);
@@ -53,6 +59,7 @@ public class LogController {
         setupTextSizeSpinner();
 
         this.noLogFilesLabel.setVisible(false);
+        this.confirmDeleteLabel.setVisible(false);
     }
 
     private void setupTextSizeSpinner() {
@@ -103,10 +110,10 @@ public class LogController {
             LogFileButton logFileButton = new LogFileButton(file.getName(), 14, 32);
 
             logFileButton.setOnMouseClicked((event) -> {
-                if (this.currentFile != null && this.currentFile.equals(file)) return;
+                if (this.currendDisplayedFile != null && this.currendDisplayedFile.equals(file)) return;
 
                 displayFileContent(file.toPath());
-                this.currentFile = file;
+                this.currendDisplayedFile = file;
             });
 
             container.add(logFileButton);
@@ -137,5 +144,59 @@ public class LogController {
         message.styleProperty().bind(
                 Bindings.concat("-fx-font-size: ", this.textSizeSpinner.valueProperty(), "px"));
         return message;
+    }
+
+    public void deleteLog() {
+        if (this.currendDisplayedFile == null) return;
+        this.confirmDeleteLabel.setVisible(false);
+
+        if (!this.deletePressedOnce) {
+            this.confirmDeleteLabel.setText("Press again to confirm deletion.");
+            this.confirmDeleteLabel.setVisible(true);
+            this.deletePressedOnce = true;
+            return;
+        }
+
+        if (this.currendDisplayedFile.delete()) {
+            this.logFileContentTextFlow.getChildren().clear();
+            this.currendDisplayedFile = null;
+            this.deletePressedOnce = false;
+
+            if (this.backupLogTButton.isSelected()) listLogsAsLogFileButton("backup");
+            else listLogsAsLogFileButton("error");
+            return;
+        }
+
+        this.confirmDeleteLabel.setText("Could not delete file.");
+        this.confirmDeleteLabel.setVisible(true);
+        this.deletePressedOnce = false;
+    }
+
+    public void exportLog() {
+        if (this.currendDisplayedFile == null) return;
+        this.confirmDeleteLabel.setVisible(false);
+
+        FileChooser destSelection = new FileChooser();
+        destSelection.setTitle("Select an export location");
+        if (!this.lastSelectedExportPath.isEmpty())
+            destSelection.setInitialDirectory(new File(this.lastSelectedExportPath));
+        destSelection.getExtensionFilters().add(new FileChooser.ExtensionFilter("txt-files (*.txt)", "*.txt"));
+        destSelection.setInitialFileName(this.currendDisplayedFile.getName());
+
+        File result = destSelection.showSaveDialog(new Stage());
+        if (result == null) return;
+
+        String exportPath = result.getAbsolutePath();
+        if (!exportPath.endsWith(".txt")) exportPath = exportPath + ".txt";
+
+        try {
+            Files.copy(this.currendDisplayedFile.toPath(), Path.of(exportPath));
+        } catch (IOException unused) {
+            this.confirmDeleteLabel.setText("Could not export file.");
+            this.confirmDeleteLabel.setVisible(true);
+            return;
+        }
+
+        this.lastSelectedExportPath = Path.of(exportPath).getParent().toString();
     }
 }
