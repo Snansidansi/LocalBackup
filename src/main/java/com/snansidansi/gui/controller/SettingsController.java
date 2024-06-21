a62a4f30a37d9b889e84f3b78b90912b794895de
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2024-06-21 18:53:12 +0200

    Added functionality for settings scene buttons.

--- src/main/java/com/snansidansi/gui/controller/SettingsController.java
+++ src/main/java/com/snansidansi/gui/controller/SettingsController.java
@@ -1,10 +1,20 @@
 package com.snansidansi.gui.controller;
 
+import com.snansidansi.gui.uielements.settingsrow.SettingsRow;
+import com.snansidansi.gui.util.SceneManager;
+import com.snansidansi.gui.util.SettingsManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingsController {
+    private List<SettingsRow> availableSettingsRows = new ArrayList<>();
+
     @FXML
     VBox mainContainer;
 
@@ -16,21 +26,43 @@ public class SettingsController {
 
     @FXML
     public void initialize() {
+        displaySettings();
+    }
+
+    private void displaySettings() {
+        var widthProperty = this.settingsScrollPane.widthProperty();
+    }
+
+    private <T extends SettingsRow> void addSettingsRow(T settingsRow) {
+        this.settingsVBox.getChildren().add(settingsRow);
+        this.availableSettingsRows.add(settingsRow);
     }
 
     public void backToConfigureBackupScene() {
-
+        try {
+            SceneManager.setConfigureBackupsScene((Stage) this.mainContainer.getScene().getWindow());
+        } catch (IOException unused) {
+        }
     }
 
     public void restoreDefaultSettings() {
-
+        SettingsManager.restoreDefaults();
+        this.settingsVBox.getChildren().clear();
+        displaySettings();
     }
 
-    public void abortChanges() {
-
+    public void discardChanges() {
+        for (SettingsRow settingsRow : this.availableSettingsRows)
+            settingsRow.restoreStandardValue();
     }
 
     public void saveChanges() {
+        for (SettingsRow settingsRow : this.availableSettingsRows) {
+            SettingsManager.changeSetting(settingsRow.getSetting(), settingsRow.getValue());
+            settingsRow.setStandardValue(settingsRow.getValue());
+        }
 
+        if (SettingsManager.applyChanges()) return;
+        displaySettings();
     }
 }
