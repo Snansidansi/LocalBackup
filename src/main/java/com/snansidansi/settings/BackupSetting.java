33789a59017d1b3ecb4d84f3ef30c90dfaf98058
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2024-06-22 15:25:57 +0200

    Added displayText and isUserChangeable property to BackupSetting enum.

--- src/main/java/com/snansidansi/settings/BackupSetting.java
+++ src/main/java/com/snansidansi/settings/BackupSetting.java
@@ -1,20 +1,33 @@
 package com.snansidansi.settings;
 
 public enum BackupSetting implements Settings {
-    NONE("none", "null", SettingType.BOOLEAN);
+    NONE("none", "Nothing", "null", SettingType.BOOLEAN, false);
 
     private final String id;
+    private final String displayText;
     private final String standardValue;
     private final SettingType settingType;
+    private final Boolean isUserChangeable;
 
-    BackupSetting(String id, String standardValue, SettingType settingType) {
+    BackupSetting(String id,
+                  String displayText,
+                  String standardValue,
+                  SettingType settingType,
+                  Boolean isUserChangeable) {
+
         this.id = id;
+        this.displayText = displayText;
         this.standardValue = standardValue;
         this.settingType = settingType;
+        this.isUserChangeable = isUserChangeable;
     }
 
     public String getID() {
         return this.id;
+    }
+
+    public String getDisplayText() {
+        return this.displayText;
     }
 
     public String getStandardValue() {
@@ -23,5 +36,9 @@ public enum BackupSetting implements Settings {
 
     public SettingType getType() {
         return this.settingType;
+    }
+
+    public Boolean getIsUserChangeable() {
+        return this.isUserChangeable;
     }
 }
